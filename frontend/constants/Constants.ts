961803d3683991ab702201e8f58dd1e440073e6e
K.Dileepa Thushan Peiris <dileepapeiris5@gmail.com>
2025-08-18 16:03:12 +0530

    Add AUTHENTICATOR_APP_ID constant
    Introduces AUTHENTICATOR_APP_ID to Constants.ts, sourcing its value from the EXPO_PUBLIC_AUTHENTICATOR_APP_ID environment variable.

--- frontend/constants/Constants.ts
+++ frontend/constants/Constants.ts
@@ -28,6 +28,8 @@ export const GOOGLE_ANDROID_CLIENT_ID =
   process.env.EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID ?? "";
 export const GOOGLE_WEB_CLIENT_ID =
   process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID ?? "";
+export const AUTHENTICATOR_APP_ID =
+  process.env.EXPO_PUBLIC_AUTHENTICATOR_APP_ID ?? "";
 
 export const SUCCESS = "success";
 export const APPS = "apps";
