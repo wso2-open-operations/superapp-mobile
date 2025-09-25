a918e0e97994791bc45ac585fa1ccdcfb45d4d54
v15a1 <visalrajapakse@gmail.com>
2025-09-25 18:50:47 +0530

    Minor refactor

--- frontend/app/micro-app.tsx
+++ frontend/app/micro-app.tsx
@@ -455,7 +455,7 @@ const MicroApp = () => {
       <Stack.Screen
         options={{
           title: shouldShowHeader ? appName : "",
-          headerShown: false,
+          headerShown: shouldShowHeader,
           headerRight: () =>
             isDeveloper &&
             shouldShowHeader && (
