66263ebee2f578661078e2f93eee0139ce22a334
e19166 <e19166@eng.pdn.ac.lk>
2025-10-13 13:33:49 +0530

    Remove sensitive info

--- superapp_admin_portal/admin_portal/admin-page/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/dist/'],
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
-};
