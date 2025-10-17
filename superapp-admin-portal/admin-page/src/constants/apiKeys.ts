80b0c52aee161e40dd36f493a446d8b457c38895
e19166 <e19166@eng.pdn.ac.lk>
2025-10-17 12:08:41 +0530

    Add license

--- superapp-admin-portal/admin-page/src/constants/apiKeys.ts
+++ superapp-admin-portal/admin-page/src/constants/apiKeys.ts
@@ -1,3 +1,19 @@
+// Copyright (c) 2025 WSO2 LLC. (https://www.wso2.com).
+//
+// WSO2 LLC. licenses this file to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 // Centralized constants for API endpoint keys used across the app and tests
 // Keeping as string literals to match keys used in api.ts ENDPOINTS
 export const API_KEYS = {
