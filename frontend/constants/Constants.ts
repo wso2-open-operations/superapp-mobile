7ac133fa4d3942c4836d56fcdf0a68d335c93031
K.Dileepa Thushan Peiris <dileepapeiris5@gmail.com>
2025-10-29 15:13:34 +0530

    Remove trailing whitespace in Constants.ts
    Deleted unnecessary trailing whitespace at the end of the Constants.ts file to improve code cleanliness.

--- frontend/constants/Constants.ts
+++ frontend/constants/Constants.ts
@@ -103,4 +103,3 @@ export const NOTIFICATION_CHANNEL_ID =
 export const NOTIFICATION_CHANNEL_NAME =
   process.env.EXPO_PUBLIC_NOTIFICATION_CHANNEL_NAME ??
   "Default SuperApp Notification Channel";
-  
