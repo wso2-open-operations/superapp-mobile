bd09c6c286fe0c91602ffdde4a13e6b6c4ba8c07
K.Dileepa Thushan Peiris <dileepapeiris5@gmail.com>
2025-11-03 10:27:28 +0530

    Remove extra newline in import section
    Deleted an unnecessary blank line before the import statements in configureAndroidNotificationIcon.ts to improve code formatting.

--- frontend/integrations/android-notifications/configureAndroidNotificationIcon.ts
+++ frontend/integrations/android-notifications/configureAndroidNotificationIcon.ts
@@ -13,7 +13,6 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-
 import { ConfigPlugin, withDangerousMod } from "@expo/config-plugins";
 import fs from "fs";
 import path from "path";
